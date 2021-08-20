1af7ef3ab7de83efb17aaf0696acb8ffd38d9553
Achmad Rivaldi <mrtampan54@gmail.com>
2021-08-20 18:16:53 +0700

    sss

---
+++ office/aktivasi.bat
@@ -0,0 +1,7 @@
+cscript ospp.vbs /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99
+cscript ospp.vbs /unpkey:BTDRB >nul
+cscript ospp.vbs /unpkey:KHGM9 >nul
+cscript ospp.vbs /unpkey:CPQVG >nul
+cscript ospp.vbs /sethst:kms8.msguides.com
+cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /act
